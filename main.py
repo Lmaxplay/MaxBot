4582d04895625d46b74947082ba80c4ba507c3fb
Lmaxplay <lmaxplay@outlook.com>
2022-03-04 19:04:18 +0100

    Commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import nextcord
+import nextcord.client
+import dotenv
+from nextcord.ext import commands
+
+bot = commands.Bot(command_prefix="$")
+
+@bot.command()
+async def ping(ctx: commands.context.Context):
+    await ctx.send("Pong!")
+
+bot.run(dotenv.get_key(".env", "TOKEN"))
