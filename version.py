b7eab34d0471bf1234cc3456d6a0356980802f93
Lmaxplay <lmaxplay@outlook.com>
2022-03-09 17:52:14 +0100

    tiny fix in version.py

--- version.py
+++ version.py
@@ -10,4 +10,4 @@ def getNextcordVersion():
     return f"{nextcord.version_info.major}.{nextcord.version_info.minor}.{nextcord.version_info.micro} {nextcord.version_info.releaselevel}"
 
 def getPythonVersion():
-    return str(sys.version_info.major) + "." + str(sys.version_info.minor) + "." + str(sys.version_info.micro) #+ " " + str(sys.version_info.releaselevel)
+    return f"{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro} {sys.version_info.releaselevel}"
